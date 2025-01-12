6ec2ef87d07fc676906933a40ef3a1a26cdd3de8
Jarryd Stanbrook <jarrydstanbrook@proton.me>
2025-01-12 11:17:30 +1100

    update ansible

--- tofu/test-cloudinit.tf
+++ tofu/test-cloudinit.tf
@@ -1,34 +1,34 @@
 
 resource "null_resource" "cloud_init_vm-terraform-test" {
   connection {
-    type    = "ssh"
-    user    = var.pve_user
+    type        = "ssh"
+    user        = var.pve_user
     private_key = file("~/.ssh/id_ed25519")
-    host    = var.pve_host
-  } 
+    host        = var.pve_host
+  }
   provisioner "file" {
-    source       = local_file.cloud_init_vm-terraform-test.filename
-    destination  = "/var/lib/vz/snippets/cloud_init_vm-terraform-test.yml"
+    source      = local_file.cloud_init_vm-terraform-test.filename
+    destination = "/var/lib/vz/snippets/cloud_init_vm-terraform-test.yml"
   }
 }
 
 # Create a local copy of the file, to transfer to Proxmox
 resource "local_file" "cloud_init_vm-terraform-test" {
   # content   = data.template_file.cloud_init_vm-terraform-test.rendered
-  content   = templatefile("${path.module}/cloud-init/pve2.tftpl", {ssh_key = file("~/.ssh/id_ed25519.pub"), hostname = var.vm_hostname, domain = var.vm_domain, tailscale_auth_key = var.tailscale_auth_key})
-  filename  = "${path.module}/files/user_data_cloud_init_vm-terraform-test.cfg"
+  content  = templatefile("${path.module}/cloud-init/pve2.tftpl", { ssh_key = file("~/.ssh/id_ed25519.pub"), hostname = var.vm_hostname, domain = var.vm_domain, tailscale_auth_key = var.tailscale_auth_key })
+  filename = "${path.module}/files/user_data_cloud_init_vm-terraform-test.cfg"
 }
 resource "proxmox_vm_qemu" "vm-terraform-test" {
-    depends_on = [
+  depends_on = [
     null_resource.cloud_init_vm-terraform-test
   ]
   count            = 1
-  vmid             = 201
+  vmid             = 100
   name             = "vm-terraform-test"
   desc             = "terraform test"
   target_node      = "pve2"
   agent            = 1
-  clone            = "ubuntu-cloud"
+  clone            = "arch-cloud"
   cores            = 2
   cpu_type         = "host"
   memory           = 4096
@@ -62,15 +62,23 @@ resource "proxmox_vm_qemu" "vm-terraform-test" {
           iothread   = true
         }
       }
+      scsi1 {
+        disk {
+          storage    = "data"
+          size       = "200G"
+          emulatessd = true
+          iothread   = true
+        }
+      }
     }
   }
   network {
-    id = 0
+    id     = 0
     model  = "virtio"
     bridge = "vmbr0"
   }
   provisioner "local-exec" {
-  command = <<EOT
+    command = <<EOT
     ssh-keygen -R 192.168.1.10
     ssh-keyscan -H 192.168.1.10 >> ~/.ssh/known_hosts
   EOT
